d81b73b372aeaa9c27c6c1ef091c6e0f2c72c8bf
SuperDuperAwesomeAmazingPerson <leo.penguin612@gmail.com>
2024-10-25 18:08:51 -0500

    added code for intake slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Summer_School_SS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Summer_School_SS.java
@@ -47,7 +47,9 @@ public class Summer_School_SS extends OpMode
     private DcMotor BRMotor = null;
     private DcMotor BLMotor = null;
 
-    GoBildaPinpointDriver odo; // Declare OpMode member for the Odometry Computer
+    private DcMotor intakie;  // Motor for the extending/retracting mechanism
+
+//    GoBildaPinpointDriver odo; // Declare OpMode member for the Odometry Computer
 
     double oldTime = 0;
 
@@ -71,6 +73,15 @@ public class Summer_School_SS extends OpMode
         BRMotor = hardwareMap.get(DcMotor.class, "BR");
         BLMotor = hardwareMap.get(DcMotor.class, "BL");
 
+        intakie = hardwareMap.get(DcMotor.class, "intakie");
+
+// Set the motor direction and zero power behavior
+
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
 
 
         FRMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -78,11 +89,15 @@ public class Summer_School_SS extends OpMode
         BRMotor.setDirection(DcMotor.Direction.REVERSE);
         BLMotor.setDirection(DcMotor.Direction.FORWARD);
 
+        intakie.setDirection(DcMotor.Direction.FORWARD);
+
 
         FLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intakie.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
 
@@ -91,53 +106,53 @@ public class Summer_School_SS extends OpMode
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
 
-        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+//        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+//
+//        /*
+//        Set the odometry pod positions relative to the point that the odometry computer tracks around.
+//        The X pod offset refers to how far sideways from the tracking point the
+//        X (forward) odometry pod is. Left of the center is a positive number,
+//        right of center is a negative number. the Y pod offset refers to how far forwards from
+//        the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
+//        backwards is a negative number.
+//         */
+//        //  odo.setOffsets(-84.0, -224.0); //these are tuned for 3110-0002-0001 Product Insight #1
+//        odo.setOffsets(-153.71, -215.019);
+//        /*
+//        Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
+//        the goBILDA_SWINGARM_POD, or the goBILDA_4_BAR_POD.
+//        If you're using another kind of odometry pod, uncomment setEncoderResolution and input the
+//        number of ticks per mm of your odometry pod.
+//         */
+//        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+//        //odo.setEncoderResolution(13.26291192);
+//
+//
+//        /*
+//        Set the direction that each of the two odometry pods count. The X (forward) pod should
+//        increase when you move the robot forward. And the Y (strafe) pod should increase when
+//        you move the robot to the left.
+//         */
+//        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+//
+//
+//        /*
+//        Before running the robot, recalibrate the IMU. This needs to happen when the robot is stationary
+//        The IMU will automatically calibrate when first powered on, but recalibrating before running
+//        the robot is a good idea to ensure that the calibration is "good".
+//        resetPosAndIMU will reset the position to 0,0,0 and also recalibrate the IMU.
+//        This is recommended before you run your autonomous, as a bad initial calibration can cause
+//        an incorrect starting value for x, y, and heading.
+//         */
+//        //odo.recalibrateIMU();
+//        odo.resetPosAndIMU();
 
-        /*
-        Set the odometry pod positions relative to the point that the odometry computer tracks around.
-        The X pod offset refers to how far sideways from the tracking point the
-        X (forward) odometry pod is. Left of the center is a positive number,
-        right of center is a negative number. the Y pod offset refers to how far forwards from
-        the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
-        backwards is a negative number.
-         */
-        //  odo.setOffsets(-84.0, -224.0); //these are tuned for 3110-0002-0001 Product Insight #1
-        odo.setOffsets(-153.71, -215.019);
-        /*
-        Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
-        the goBILDA_SWINGARM_POD, or the goBILDA_4_BAR_POD.
-        If you're using another kind of odometry pod, uncomment setEncoderResolution and input the
-        number of ticks per mm of your odometry pod.
-         */
-        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
-        //odo.setEncoderResolution(13.26291192);
-
-
-        /*
-        Set the direction that each of the two odometry pods count. The X (forward) pod should
-        increase when you move the robot forward. And the Y (strafe) pod should increase when
-        you move the robot to the left.
-         */
-        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
-
-
-        /*
-        Before running the robot, recalibrate the IMU. This needs to happen when the robot is stationary
-        The IMU will automatically calibrate when first powered on, but recalibrating before running
-        the robot is a good idea to ensure that the calibration is "good".
-        resetPosAndIMU will reset the position to 0,0,0 and also recalibrate the IMU.
-        This is recommended before you run your autonomous, as a bad initial calibration can cause
-        an incorrect starting value for x, y, and heading.
-         */
-        //odo.recalibrateIMU();
-        odo.resetPosAndIMU();
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.addData("X offset", odo.getXOffset());
-        telemetry.addData("Y offset", odo.getYOffset());
-        telemetry.addData("Device Version Number:", odo.getDeviceVersion());
-        telemetry.addData("Device Scalar", odo.getYawScalar());
-        telemetry.update();
+//        telemetry.addData("Status", "Initialized");
+//        telemetry.addData("X offset", odo.getXOffset());
+//        telemetry.addData("Y offset", odo.getYOffset());
+//        telemetry.addData("Device Version Number:", odo.getDeviceVersion());
+//        telemetry.addData("Device Scalar", odo.getYawScalar());
+//        telemetry.update();
     }
 
     @Override
@@ -146,6 +161,8 @@ public class Summer_School_SS extends OpMode
     double forward = Math.pow(gamepad1.left_stick_y, 3);
     double right = Math.pow(gamepad1.left_stick_x, 3);
     double turn = Math.pow(gamepad1.right_stick_x, 3);
+// Get the left stick Y-axis input from gamepad1 (values range from -1 to 1)
+    double extendArm = gamepad2.left_stick_y;
 
 
     double FLPower = speedMode * forward - right - turn;
@@ -154,26 +171,26 @@ public class Summer_School_SS extends OpMode
     double BRPower = speedMode * forward - right + turn;
     double[] powers = {FLPower, BLPower, FRPower, BRPower};
 
-    /*
-        Request an update from the Pinpoint odometry computer. This checks almost all outputs
-        from the device in a single I2C read.
-         */
-    odo.update();
-
-        /*
-        Optionally, you can update only the heading of the device. This takes less time to read, but will not
-        pull any other data. Only the heading (which you can pull with getHeading() or in getPosition().
-         */
-    //odo.update(GoBildaPinpointDriver.readData.ONLY_UPDATE_HEADING);
-
-
-    if (gamepad1.a){
-        odo.resetPosAndIMU(); //resets the position to 0 and recalibrates the IMU
-    }
-
-    if (gamepad1.b){
-        odo.recalibrateIMU(); //recalibrates the IMU without resetting position
-    }
+//    /*
+//        Request an update from the Pinpoint odometry computer. This checks almost all outputs
+//        from the device in a single I2C read.
+//         */
+//    odo.update();
+//
+//        /*
+//        Optionally, you can update only the heading of the device. This takes less time to read, but will not
+//        pull any other data. Only the heading (which you can pull with getHeading() or in getPosition().
+//         */
+//    //odo.update(GoBildaPinpointDriver.readData.ONLY_UPDATE_HEADING);
+//
+//
+//    if (gamepad1.a){
+//        odo.resetPosAndIMU(); //resets the position to 0 and recalibrates the IMU
+//    }
+//
+//    if (gamepad1.b){
+//        odo.recalibrateIMU(); //recalibrates the IMU without resetting position
+//    }
 
         /*
         This code prints the loop frequency of the REV Control Hub. This frequency is effected
@@ -186,37 +203,42 @@ public class Summer_School_SS extends OpMode
     double frequency = 1/loopTime;
     oldTime = newTime;
 
-
-        /*
-        gets the current Position (x & y in mm, and heading in degrees) of the robot, and prints it.
-         */
-    Pose2D pos = odo.getPosition();
-    String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
-    telemetry.addData("Position", data);
-
-        /*
-        gets the current Velocity (x & y in mm/sec and heading in degrees/sec) and prints it.
-         */
-    Pose2D vel = odo.getVelocity();
-    String velocity = String.format(Locale.US,"{XVel: %.3f, YVel: %.3f, HVel: %.3f}", vel.getX(DistanceUnit.MM), vel.getY(DistanceUnit.MM), vel.getHeading(AngleUnit.DEGREES));
-    telemetry.addData("Velocity", velocity);
-
-
-        /*
-        Gets the Pinpoint device status. Pinpoint can reflect a few states. But we'll primarily see
-        READY: the device is working as normal
-        CALIBRATING: the device is calibrating and outputs are put on hold
-        NOT_READY: the device is resetting from scratch. This should only happen after a power-cycle
-        FAULT_NO_PODS_DETECTED - the device does not detect any pods plugged in
-        FAULT_X_POD_NOT_DETECTED - The device does not detect an X pod plugged in
-        FAULT_Y_POD_NOT_DETECTED - The device does not detect a Y pod plugged in
-        */
-    telemetry.addData("Status", odo.getDeviceStatus());
-
-    telemetry.addData("Pinpoint Frequency", odo.getFrequency()); //prints/gets the current refresh rate of the Pinpoint
-
-    telemetry.addData("REV Hub Frequency: ", frequency); //prints the control system refresh rate
+    //Move forward
+    intakie.setPower(extendArm);
+    // Telemetry to monitor arm motor power
+    telemetry.addData("Arm Motor Power Extend", extendArm);
     telemetry.update();
+
+//        /*
+//        gets the current Position (x & y in mm, and heading in degrees) of the robot, and prints it.
+//         */
+//    Pose2D pos = odo.getPosition();
+//    String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
+//    telemetry.addData("Position", data);
+//
+//        /*
+//        gets the current Velocity (x & y in mm/sec and heading in degrees/sec) and prints it.
+//         */
+//    Pose2D vel = odo.getVelocity();
+//    String velocity = String.format(Locale.US,"{XVel: %.3f, YVel: %.3f, HVel: %.3f}", vel.getX(DistanceUnit.MM), vel.getY(DistanceUnit.MM), vel.getHeading(AngleUnit.DEGREES));
+//    telemetry.addData("Velocity", velocity);
+//
+//
+//        /*
+//        Gets the Pinpoint device status. Pinpoint can reflect a few states. But we'll primarily see
+//        READY: the device is working as normal
+//        CALIBRATING: the device is calibrating and outputs are put on hold
+//        NOT_READY: the device is resetting from scratch. This should only happen after a power-cycle
+//        FAULT_NO_PODS_DETECTED - the device does not detect any pods plugged in
+//        FAULT_X_POD_NOT_DETECTED - The device does not detect an X pod plugged in
+//        FAULT_Y_POD_NOT_DETECTED - The device does not detect a Y pod plugged in
+//        */
+//    telemetry.addData("Status", odo.getDeviceStatus());
+//
+//    telemetry.addData("Pinpoint Frequency", odo.getFrequency()); //prints/gets the current refresh rate of the Pinpoint
+//
+//    telemetry.addData("REV Hub Frequency: ", frequency); //prints the control system refresh rate
+//    telemetry.update();
 
     if (gamepad1.left_trigger > 0.5) {
         speedMode = 0.4;
