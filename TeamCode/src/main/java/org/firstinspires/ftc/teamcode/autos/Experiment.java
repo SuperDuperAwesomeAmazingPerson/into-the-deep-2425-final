4d675d7c2518bc18f15b6dfa25574a39ee1860df
SuperDuperAwesomeAmazingPerson <leo.penguin612@gmail.com>
2024-12-25 20:25:29 -0600

    pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Experiment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Experiment.java
@@ -40,31 +40,6 @@
  import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
  import org.firstinspires.ftc.teamcode.notUsing.GoBildaPinpointDriver;
 
- /*
-  * This OpMode illustrates the concept of driving a path based on encoder counts.
-  * The code is structured as a LinearOpMode
-  *
-  * The code REQUIRES that you DO have encoders on the wheels,
-  *   otherwise you would use: RobotAutoDriveByTime;
-  *
-  *  This code ALSO requires that the drive Motors have been configured such that a positive
-  *  power command moves them forward, and causes the encoders to count UP.
-  *
-  *   The desired path in this example is:
-  *   - Drive forward for 48 inches
-  *   - Spin right for 12 Inches
-  *   - Drive Backward for 24 inches
-  *   - Stop and close the claw.
-  *
-  *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
-  *  that performs the actual movement.
-  *  This method assumes that each movement is relative to the last stopping place.
-  *  There are other ways to perform encoder based moves, but this method is probably the simplest.
-  *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
-  *
-  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
-  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
-  */
 
  @Autonomous(name="Experiment", group="Robot")
 
@@ -100,12 +75,9 @@
      static final double     DRIVE_SPEED             = 0.575;
      static final double     TURN_SPEED              = 0.5;
      static final double     HEADING_THRESHOLD       = 5.0 ;    // How close must the heading get to the target before moving to next step.
-     // Requiring more accuracy (a smaller number) will often make the turn take longer to get into the final position.
-     // Define the Proportional control coefficient (or GAIN) for "heading control".
-     // We define one value when Turning (larger errors), and the other is used when Driving straight (smaller errors).
-     // Increase these numbers if the heading does not correct strongly enough (eg: a heavy robot or using tracks)
-     // Decrease these numbers if the heading does not settle on the correct value (eg: very agile robot with omni wheels)
+
      @Override
+
      public void runOpMode() {
 
          // Initialize the drive system variables.
